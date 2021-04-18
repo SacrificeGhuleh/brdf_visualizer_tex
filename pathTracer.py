691db41af5562498d57f03482ae4370c574c30ef
Richard Zvonek <zvonek.r@seznam.cz>
2021-04-18 21:46:17 +0200

    Oprava chyb, reorganizace, doplnění obrázků, akcelerační struktury

--- pathTracer.py
+++ pathTracer.py
@@ -1,15 +1,18 @@
 def trace(ray):
+  if sampleLight:
+    sampleLight = False
+    return sampleRandomLight()
+  else
+    sampleLight = True
     # Russian roulette
     if currentRecursion > 2 and (albedo * 0.95) <= rng():
-        return 0
+    return 0
 
     if material.isLight():
-        return material.emission
-
-    Ld = sampleRandomLight()
+      return material.emission
 
     omegaI, pdf = sample(normal)
     brdf = getBrdf(omegaI, normal)
     newHit = generateRay(worldPos, omegaI)
     Li = trace(newHit, currentRecursion + 1)
-    return Li * brdf * dot(omegaI, normal) / (pdf * (albedo * 0.95)) + Ld
+    return Li * brdf * dot(omegaI, normal) / (pdf * (albedo * 0.95))
